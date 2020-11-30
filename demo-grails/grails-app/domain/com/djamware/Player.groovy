19056d17e61461040b0eedb4bc5ce500b3a36a98
Tphorton <Tphorton14@gmail.com>
2020-11-29 20:31:44 -0500

    adding to grails domain

--- demo-grails/grails-app/domain/com/djamware/Player.groovy
+++ demo-grails/grails-app/domain/com/djamware/Player.groovy
@@ -2,6 +2,19 @@ package com.djamware
 
 class Player {
 
+    static belongsTo = [item: Team]
+    String name
+    Integer age
+    String position
+
     static constraints = {
+
+        name size: 5..40, blank: false
+        age min: 16
+        position size: 5..40, blank: false
+    }
+
+    Strings toString () {
+        name
     }
 }
